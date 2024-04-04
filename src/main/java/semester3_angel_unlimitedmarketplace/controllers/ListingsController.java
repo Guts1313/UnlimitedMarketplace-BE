3e6eaf73226a0b254f833b70d1d20bfdc65723d0
Angel Rusev <i517171@FontysICT.nl>
2024-04-04 23:19:20 +0200

    docker

--- src/main/java/semester3_angel_unlimitedmarketplace/controllers/ListingsController.java
+++
@@ -1,9 +0,0 @@
-package semester3_angel_unlimitedmarketplace.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/unlimitedmarketplace/lisings")
-public class ListingsController {
-}
