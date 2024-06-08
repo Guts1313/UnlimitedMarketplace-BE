cdadd1ecb8d29c0d518e9977c8cde63253cdcf27
Angel Rusev <i517171@FontysICT.nl>
2024-06-09 01:10:38 +0200

    deployment

--- unlimitedmarketplace-V2/unlimitedmarketplace/build.gradle.kts
+++ unlimitedmarketplace-V2/unlimitedmarketplace/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	implementation("com.auth0:java-jwt:$javaAuthJwt")
 	implementation("io.jsonwebtoken:jjwt-impl:$jsonWebTokenApiJacksonVersion")
 	implementation("org.springframework.boot:spring-boot-starter-security")
