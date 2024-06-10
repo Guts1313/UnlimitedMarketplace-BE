c93fe4353e1061b19c1d600a1690ada878bffe33
Angel Rusev <i517171@FontysICT.nl>
2024-06-10 13:05:55 +0200

    d

--- unlimitedmarketplace-V2/unlimitedmarketplace/src/main/java/unlimitedmarketplace/persistence/UserRepository.java
+++ unlimitedmarketplace-V2/unlimitedmarketplace/src/main/java/unlimitedmarketplace/persistence/UserRepository.java
@@ -1,8 +1,6 @@
 package unlimitedmarketplace.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import unlimitedmarketplace.persistence.entity.UserEntity;
 
 import java.util.List;
@@ -10,13 +8,8 @@ import java.util.Optional;
 
 
 public interface UserRepository  extends JpaRepository<UserEntity,Long> {
-    @Query("SELECT u FROM UserEntity u WHERE u.userName = :userName")
-    List<UserEntity> findAllByUserName(@Param("userName") String userName);
-
-    @Query("SELECT u FROM UserEntity u WHERE u.userName = :userName")
-    Optional<UserEntity> findByUserName(@Param("userName") String userName);
-
-    @Query("SELECT u FROM UserEntity u WHERE u.email = :email")
-    Optional<UserEntity>findByEmail(@Param("email") String email);
+    List<UserEntity> findAllByUserName(String userName);
+    Optional<UserEntity> findByUserName(String userName);
+    Optional<UserEntity> findByEmail(String email);
 
 }
